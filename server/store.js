7a477b7eef34dda9a16aa194206a454b019bf902
alberto hortelano <alberto.hortelano.moran@gmail.com>
2017-10-22 20:07:23 +0100

    test commit

--- server/store.js
+++ server/store.js
@@ -3,9 +3,9 @@ import { createStore, combineReducers } from 'redux';
 import { click } from './reducers';
 
 const store = createStore(
- 	combineReducers({ click }),
-	/* preloadedState, */
-	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	combineReducers({ click })/*,
+	preloadedState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
 );
 
 export default store;
